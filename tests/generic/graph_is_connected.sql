7b7a5e60dfef21b5004a1179654e2d86e3c5c470
James McNeill <james.mcneill@conjura.com>
2022-07-21 09:11:10 +0100

    add graph is connected generic test

---
+++ tests/generic/graph_is_connected.sql
@@ -0,0 +1,31 @@
+{% test graph_is_connected(
+    model,
+    edge_id,
+    vertex_1,
+    vertex_2,
+    graph_id=none
+) %}
+
+with connected_subgraphs as (
+    {{ dbt_graph_theory.subgraph_identifier(
+        input=model,
+        edge_id=edge_id,
+        vertex_1=vertex_1,
+        vertex_2=vertex_2,
+        graph_id=graph_id
+    ) }}
+),
+
+subgraphs_per_graph as (
+    select
+        {{ graph_id ~ ',' if graph_id }}
+        count(distinct subgraph_id) as num_subgraphs
+    from
+        connected_subgraphs
+    {{ 'group by ' ~ graph_id if graph_id }}
+)
+
+select * from subgraphs_per_graph
+where num_subgraphs != 1
+
+{% endtest %}
