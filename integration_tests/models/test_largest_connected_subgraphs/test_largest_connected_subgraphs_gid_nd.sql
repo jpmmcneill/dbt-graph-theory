7b929204d155494d1836a70652f3b0164d8e6d86
James McNeill <james.mcneill@conjura.com>
2023-06-01 14:37:00 +0100

    linty sminty

--- integration_tests/models/test_largest_connected_subgraphs/test_largest_connected_subgraphs_gid_nd.sql
+++ integration_tests/models/test_largest_connected_subgraphs/test_largest_connected_subgraphs_gid_nd.sql
@@ -8,7 +8,12 @@ with computed as (
 required as (
     select v.* from (
         values
-        (cast(null as {{ type_string() }}), cast(null as {{ type_string() }}), cast(null as {{ type_string() }}), array[null])
+        (
+            cast(null as {{ type_string() }}),
+            cast(null as {{ type_string() }}),
+            cast(null as {{ type_string() }}),
+            array[null]
+        )
     ) as v (graph_id, vertex, subgraph_id, subgraph_members)
     where false
 )
